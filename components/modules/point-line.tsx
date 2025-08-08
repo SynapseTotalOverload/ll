ba991a300c79e1454174afc629fd65f9536c1b6b
freelifestyleman <pylypiaklvivwork@gmail.com>
2025-08-08 13:46:02 +0300

    Enhance PointLine component layout and styling
    - Added a container reference to dynamically adjust the height of the vertical line.
    - Updated the layout to improve responsiveness and visual alignment.
    - Changed text color for better contrast and readability.

--- components/modules/point-line.tsx
+++ components/modules/point-line.tsx
@@ -1,14 +1,18 @@
+import { useRef } from "react";
+
 export function PointLine({ title, description, index }: { title: string; description: string; index: number }) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div className="flex flex-row gap-[12px]">
+    <div className="flex max-w-[60%] flex-row gap-[12px]">
       <div className="flex flex-col items-center">
-        <div className="bg-night-blue outline-night-blue aspect-square size-4 rounded-full outline-[1px] outline-offset-[4px]" />
-        <div className="item-center flex w-full flex-col">
-          <div className="bg-night-blue h-full w-[1px]" />
+        <div className="bg-night-blue outline-night-blue mt-2 aspect-square size-4 rounded-full outline-[1px] outline-offset-[4px]" />
+        <div className="relative flex w-full flex-col items-center">
+          <div className="bg-night-blue w-[1px]" style={{ height: `${containerRef.current?.clientHeight}px` }} />
         </div>
       </div>
-      <div className="mb-8 flex flex-col gap-2">
-        <span className="text-charcoal text-[20px] font-[700]">
+      <div ref={containerRef} className="mb-8 flex flex-col gap-2">
+        <span className="text-plastic text-[20px] font-[700]">
           {index}. {title}
         </span>
         <p className="text-[24px] font-[400] text-white">{description}</p>
